e96b63232e58546562da5bd8d25d08678f4abaf6
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2023-02-23 12:56:06 +0100

    Add files via upload

---
+++ Videogiochi coconut/videogiochi.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Videogiochi_coconut
+{
+    public class Personaggio
+    {
+        public string nome { get; protected set; }
+        public int vita { get; protected set; }
+        protected Random rnd;
+
+        public Personaggio(string nome)
+        {
+            this.nome = nome;
+            vita = rnd.Next(1, 101);
+        }
+    }
+    public class Guerriero : Personaggio
+    {
+        int Forza;
+        public Guerriero(string nome) : base(nome)
+        {
+            Forza = rnd.Next(1, 101);
+        }
+        public string IL_METODO()
+        {
+            return $"{nome},{vita},{Forza};";
+        }
+    }
+    public class Mago : Personaggio
+    {
+        int Mana;
+        public Mago(string nome) : base(nome)
+        {
+            Mana = rnd.Next(1, 101);
+        }
+        public string IL_METODO()
+        {
+            return $"{nome},{vita},{Mana};";
+        }
+    }
+    public class Curatore : Personaggio
+    {
+        int Cura;
+        public Curatore(string nome) : base(nome)
+        {
+            Cura = rnd.Next(1, 101);
+        }
+        public string IL_METODO()
+        {
+            return $"{nome},{vita},{Cura};";
+        }
+    }
+    public class Boss
+    {
+        public string nome { get; protected set; }
+        public int vita { get; protected set; }
+        public int Attacco { get; protected set; }
+        protected Random rnd;
+        public Boss(string nome)
+        {
+            this.nome = nome;
+            vita = 200;
+            Attacco = rnd.Next(1, 301);
+        }
+        public string IL_METODO()
+        {
+            return $"{nome},{vita},{Attacco};";
+        }
+    }
+}
---
+++ mainino/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Videogiochi_coconut;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mainino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personaggio[] partY = new Personaggio[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Che personaggio vuole n°{i + 1}");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine($"‮‮‮‮‮‮Come vuole chiamare il carratere n°{i + 1}");
+                        partY[i] = new Mago(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine($"‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮‮Come vuole chiamare il carratere n°{i + 1}");
+                        partY[i] = new Guerriero(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine($"‮‮‮‮‮‮Come vuole chiamare il carratere n°{i + 1}");
+                        partY[i] = new Curatore(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("He, He, He, Haw");
+                        i--;
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
